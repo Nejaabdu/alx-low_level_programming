037df93b814cf1b74c3eaedf493d1c542309f83a
Nejaabdu <nejaabdu33@gmail.com>
2023-02-21 09:23:54 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,21 +7,20 @@
  */
 
 int print_sign(int n)
-
 {
 	if (n > 0)
 	{
-	_putchar(43);
-	return (1);
+		_putchar(43);
+		return (1);
 	}
-       	else if (n < 0)
+	else if (n < 0)
 	{
-	_putchar(45);
-	return (-1);
+		_putchar(45);
+		return (-1);
 	}
-	else 
+	else
 	{
-	_putchar(48);
-	return (0);
+		_putchar(48);
+		return (0);
 	}
 }
