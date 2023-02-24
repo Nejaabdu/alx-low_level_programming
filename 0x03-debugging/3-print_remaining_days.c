8435666f897ecc5e4c638fa6a1656a5d0929dbaa
Nejaabdu <nejaabdu33@gmail.com>
2023-02-24 09:22:58 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,15 +10,17 @@
 * Return: void
 */
 
+
 void print_remaining_days(int month, int day, int year)
+
 {
+
 if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 {
 if (month >= 2 && day >= 60)
 {
 day++;
 }
-
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
@@ -35,4 +37,3 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
-
