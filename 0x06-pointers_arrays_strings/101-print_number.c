c762e3b5650acf84de80b7ee30dc18e968eff4a2
Nejaabdu <yusratravel11@gmail.com>
2023-03-02 20:53:37 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * prnt_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ */
+
+void print_number(int n);
+{
+	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	putchar((n1 % 10) + '0');
+}
