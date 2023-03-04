9abf9b1f9393e6e2d32a7c9e841d4f42b61d8fae
Nejaabdu <yusratravel11@gmail.com>
2023-03-04 10:49:41 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
- *
  * Return: Always 0 (success)
  */
 
 int main(void)
+
 {
-	int c;
-	int d;
-	int e = 0;
+	int d, p, q;
 
-	while (e < 10)
+	for (d = '0'; d < '9'; d++)
 	{
-		d = 0;
-		while (c < 10)
-		{
-			if (c != d && d != e && e < d && d < c)
-			{
-				putchar('0' + e);
-				putchar('0' + d);
-				putchar('0' + c);
 
-				if (c + d + e != 9 + 8 + 7)
+		for (p = d + 1; p <= '9'; p++)
+		{
+
+			for (q = p + 1; q <= '9'; q++)
+			{
+				if ((p != d) != q)
 				{
+					putchar(d);
+					putchar(p);
+					putchar(q);
+					if (d == '7' && p == '8')
+						continue;
 					putchar(',');
 					putchar(' ');
 				}
-				e++;
 			}
-			d++;
 		}
-		c++;
 	}
 	putchar('\n');
 	return (0);
 }
-
-
