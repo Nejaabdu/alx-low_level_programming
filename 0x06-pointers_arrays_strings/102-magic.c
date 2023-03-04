8fd36d5c680acd3f1c0d5dc925f935c7feae4fe6
Nejaabdu <yusratravel11@gmail.com>
2023-03-04 12:30:30 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statment
+ * - you are not allowed to code anything else than this line of code
+ */
+int main(void)
+{
+	int n;
+	int a[s];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+*(p + s) = 98;
+/* ... so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -13,5 +16,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
+int main(void);
 #endif
