7600a67bfbf81f3a0110af1922520ec8a1ff7fe3
Nejaabdu <yusratravel11@gmail.com>
2023-03-06 11:29:50 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,23 +8,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
-
-	value = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		check = 0;
-
-		for (n = 0; accept[n] != '\0'; n++)
-		{
-			if (accept[n] == s[i])
-			{
-				value++;
-				check = 1;
-			}
+        unsigned int n = 0;
+        int r;
+        
+        while (*s)
+        {       
+                for (r = 0; accept[r]; r++)
+                {       
+                        if (*s == accept[r])
+                        {       
+                                n++;
+                                break;
+                        }       
+                        else if (accept[r + 1] == '\0')
+				return (n);
 		}
-		check++;
+		s++;
 	}
-	return (check);
+	return (n);
 }
